3022ec3ac07a93112655455f32df1f6182d71e30
trw-gewang <57048074+trw-gewang@users.noreply.github.com>
2024-05-30 06:32:43 -0700

    Add files via upload

--- test.pl
+++ test.pl
@@ -1,9 +1,10 @@
 #!/perl/bin/perl -w
 
-use DateTime;
+#use DateTime;
+use HTTP::Tiny;
 open(OUTFILE,">my_test.txt") or die "Could not open Data.txt.\n";
 
-print OUTFILE "this is a test.  current time: ", DateTime->now;
+print OUTFILE "this is a test.  current time: ", time();
 
 close(OUTFILE);
 exit();
